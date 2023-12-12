fadcecc65a183f2152d3f80948f3ecb4fc1c4f25
ncar285 <carlier.nicholas@gmail.com>
2023-12-12 23:20:24 +1300

    fix bug

--- assets/main.js
+++ assets/main.js
@@ -24,15 +24,21 @@ document.querySelector('.find-people').addEventListener('click', () => {
 
     if (matches) {
         alert(`Your people to gift are: ${matches[0]} and ${matches[1]}\n
-        don't but the same gift as ${coGifters[0]} for ${matches[0]}\n
-        don't but the same gift as ${coGifters[1]} for ${matches[1]}\n`);
+        don't buy the same gift as ${coGifters[0]} for ${matches[0]}\n
+        don't buy the same gift as ${coGifters[1]} for ${matches[1]}\n
+        `);
     } else {
         alert('Name not found in the participants list.');
     }
 });
 
 function deterministicDayShuffle(name) {
-    date = new Date('2001-01-15')
+    // actual date:
+    date = new Date('2001-01-15');
+
+    // developement date:
+    // date = new Date('2011-02-12');
+
 	const uniqueDayCode = (date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate()) * (date.getMonth() + 1 + date.getDate());
 	let people = ['nico', 'josh', 'amanda', 'philippe', 'elaine', 'bruce', 'xavier'];
 	if (!people.includes(name)) return null;
@@ -44,20 +50,20 @@ function deterministicDayShuffle(name) {
 		people = people.filter((v,i) => i !== index);
 	}
 
-    const shift = (uniqueDayCode ) % shuffled.length;
-    const shiftedPeople = [...shuffled.slice(shift), ...shuffled.slice(0, shift)];
-    const index = shiftedPeople.indexOf(name);
-    const person1 = shiftedPeople[(index + 1) % (shuffled.length)];
-    const person2 = shiftedPeople[(index + 2) % (shuffled.length)];
 
+    const indexOfPerson = shuffled.indexOf(name)
 
-    const coGifter1Index = (shiftedPeople.indexOf(person1) - 2 + shuffled.length) % shuffled.length;
-    const coGifter2Index = (shiftedPeople.indexOf(person1) - 1 + shuffled.length) % shuffled.length;
-    const coGifter1 = shiftedPeople[coGifter1Index];
-    const coGifter2 = shiftedPeople[coGifter2Index];
+    let sorted = [];
+    if (indexOfPerson === 0){
+        sorted = [shuffled[shuffled.length - 1], ...shuffled.slice(0,shuffled.length - 1)];
+    } else if (indexOfPerson === 1){
+        sorted = shuffled;
+    } else {
+        sorted = [...shuffled.slice(indexOfPerson - 1), ...shuffled.slice(0, indexOfPerson - 1)]
+    }
 
-    const matches = [person1, person2];
-    const coGifters = [coGifter1, coGifter2];
+    const matches = [sorted[2], sorted[3]];
+    const coGifters = [sorted[0], sorted[2]];
  
     return [matches, coGifters];
 }
